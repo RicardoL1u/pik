dfd394b93e74a7b2dcc59109b8f02b98b91d9da0
RicardoL1u <2018212874@bupt.edu.cn>
2024-01-11 23:33:37 +0800

    fea: Add generation scripts for llama models

---
+++ scripts/generation/g7-2.sh
@@ -0,0 +1,14 @@
+export CUDA_VISIBLE_DEVICES=6
+
+model=llama-2-7b
+datafolder=data/$model
+mkdir -p $datafolder
+python generation_lyt.py \
+    --model_checkpoint MODELS/$model \
+    --n_answers_per_question 30 \
+    --max_new_tokens 16 \
+    --temperature 1 \
+    --pad_token_id 50256 \
+    --keep_all_hidden_layers \
+    --hidden_states_filename $datafolder/hidden_states.pt \
+    --text_generations_filename $datafolder/text_generations.csv
