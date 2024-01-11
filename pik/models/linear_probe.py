acc626a4a00ac631b6deb786ffad6251b10a8778
RicardoL1u <2018212874@bupt.edu.cn>
2024-01-11 19:59:14 +0800

    ref: don't use the tab as indent

--- pik/models/linear_probe.py
+++ pik/models/linear_probe.py
@@ -1,14 +1,12 @@
 import torch.nn as nn
 
 class LinearProbe(nn.Module):
-	def __init__(self, dims):
-		super().__init__()
-		self.model = nn.Sequential(
-			nn.Linear(dims, 1),
-			# nn.ReLU(),
-			# nn.Linear(512, 1),
-			nn.Sigmoid(),
-		)
+    def __init__(self, dims):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(dims, 1),
+            nn.Sigmoid(),
+        )
 
-	def forward(self, x):
-		return self.model(x)
+    def forward(self, x):
+        return self.model(x)
