782778e3de28739bb15e96e9fc86e24ffc0b3805
RicardoL1u <2018212874@bupt.edu.cn>
2024-01-22 18:09:26 +0800

    fea: evaluate the trained model on other dataset

--- evaluate.py
+++ evaluate.py
@@ -1,69 +1,89 @@
-'''
-Evaluate a model on the TriviaQA validation set (zero-shot).
-'''
+from pik.datasets.direct_hidden_states_dataset import DirectHiddenStatesDataset
+from pik.models.linear_probe import LinearProbe
 import argparse
+import logging
 import torch
-from tqdm import trange
-from pik.datasets.triviaqa_dataset import TriviaQADataset
-from pik.models.model import Model
-from pik.utils import evaluate_answer, prompt_eng
+import numpy as np
+import matplotlib.pyplot as plt
+from try_to_plot import plot_calibration
 
-# Set params
-parser = argparse.ArgumentParser()
-parser.add_argument('--n_questions', type=int, default=1000, help='number of q-a pairs to evaluate, index of selected questions is saved in `data_ids`; if <= 0, all questions are used')
-parser.add_argument('--dataset_seed', type=int, default=420, help='seed for selecting questions from dataset')
-parser.add_argument('--model_checkpoint', '-m', default='EleutherAI/gpt-j-6B', help='model checkpoint to use')
-parser.add_argument('--precision', '-p', default='float16', help='model precision')
-parser.add_argument('--n_answers_per_question', type=int, default=10, help='number of answers to generate per question')
-parser.add_argument('--max_new_tokens', type=int, default=16, help='maximum number of tokens to generate per answer')
-parser.add_argument('--temperature', type=float, default=1, help='temperature for generation')
-parser.add_argument('--pad_token_id', type=int, default=50256, help='pad token id for generation')
-parser.add_argument('--verbose', '-v', action='store_true', default=False, help='print more info')
-args = parser.parse_args()
-args.precision = torch.float16 if args.precision == 'float16' else torch.float32
-torch.set_default_dtype(args.precision)
-generation_options = {
-	'pad_token_id': args.pad_token_id,
-	'max_new_tokens': args.max_new_tokens,
-    'do_sample': True,
-    'temperature': args.temperature,
-}
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+def parse_arguments():
+    def parse_layers(arg):
+        if arg.lower() == 'none':
+            return None
+        try:
+            return [int(layer.strip()) for layer in arg.split(',')]
+        except ValueError:
+            raise argparse.ArgumentTypeError("Value must be 'None' or a comma-separated list of integers")
+    parser = argparse.ArgumentParser(description='Evaluate a linear probe on a dataset.')
+    parser.add_argument('--dataset', required=True, choices=['gsm8k','trivia_qa'], help='dataset name')
+    parser.add_argument('--model_ckpt_path', required=True, help='path to model checkpoint')
+    parser.add_argument('--hidden_states_filename', default='hidden_states.pt', help='filename for saving hidden states')
+    parser.add_argument('--text_generations_filename', default='text_generations.csv', help='filename for saving text generations')
+    parser.add_argument('--debug', action='store_true', help='Debug mode.')
+    parser.add_argument('--model_layer_idx', default=None, type=parse_layers,
+                    help='Model layer index(es), which layer(s) to use. None for all layers, \
+                    or specify indices separated by commas (e.g., 0,2,4).')
+    args = parser.parse_args()
+    return args
 
-# Load dataset and model
-data = TriviaQADataset()
-torch.manual_seed(args.dataset_seed)
-data_ids = torch.randperm(len(data))
-if args.n_questions > 0:
-	data_ids = data_ids[:args.n_questions]
-data_ids = data_ids.cpu().numpy().tolist()
-model = Model(args.model_checkpoint, precision=args.precision)
+def setup_logging(args):
+    logger = logging.getLogger(__name__)
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    # logging all the args
+    logging.info("===== Args =====")
+    for arg in vars(args):
+        logging.info("{}: {}".format(arg, getattr(args, arg)))
+    return logger
 
-# Loop through each question-answer pair in the validation set
-tot = 0
-correct = 0
-progress_bar = trange(len(data_ids), position=0, leave=True)
-for i in progress_bar:
-	# Get the question and the correct answer
-	hid, qid = i, data_ids[i]
-	question, correct_answer = data[qid]
-	desc = ''
-	if args.verbose:
-		desc += f'Question: {question}\n'
-		desc += f'Correct answer: {correct_answer}\n'
-	text_input = prompt_eng(question)
-	# Generate multiple model answers
-	for n in range(args.n_answers_per_question):
-		model_answer = model.get_text_generation(text_input, generation_options=generation_options)
-		eval = evaluate_answer(model_answer, correct_answer)
-		if eval:
-			if args.verbose:
-				desc += f'Correct! Model answer: {model_answer}\n'
-			correct += 1
-			break
-	tot += 1
-	desc += f'Question {i+1}/{len(data_ids)} - Accuracy: {correct/tot}'
-	if args.verbose:
-		desc += '\n\n'
-		desc += '=' * 80
-		desc += '\n'
-	progress_bar.set_description(desc)
+if __name__ == '__main__':
+    
+    torch.set_default_dtype(torch.float16)
+    
+    args = parse_arguments()
+    logger = setup_logging(args)
+    logging.info("Loading dataset from {} and {}" \
+                 .format(args.hidden_states_filename, args.text_generations_filename))
+    dataset = DirectHiddenStatesDataset(
+        hs_file = args.hidden_states_filename, 
+        tg_file = args.text_generations_filename,
+        layer_idx = args.model_layer_idx
+    )
+    model = LinearProbe(dims=dataset.hidden_states.shape[1]).to('cuda')
+    logging.info("Loading model checkpoint from {}".format(args.model_ckpt_path))
+    try:
+        model.load_state_dict(torch.load(args.model_ckpt_path))
+    except:
+        logging.info("Loading model checkpoint from {} failed, \
+                     trying to convert the keys".format(args.model_ckpt_path))
+        # convert the keys to from ln.weight to model.0.weight and ln.bias to model.0.bias
+        ckpt_dict = torch.load(args.model_ckpt_path)
+        new_ckpt_dict = {}
+        for k, v in ckpt_dict.items():
+            new_ckpt_dict['model.0.'+k.replace('ln.','')] = v
+        model.load_state_dict(new_ckpt_dict)
+    # evaluate
+    model.eval()
+    with torch.no_grad():
+        preds = model(dataset.hidden_states).detach().cpu().numpy().squeeze()
+    labels = dataset.pik_labels
+    
+    # calculate brier score
+    brier_score = np.mean((preds - labels)**2)
+    logging.info("Brier score: {}".format(brier_score))    
+    
+    # plt the scatter plot
+    figure_path = args.model_ckpt_path.replace('.pt', args.dataset+'_scatter.png')
+    plt.figure(figsize=(8,8))
+    plt.scatter(labels, preds, alpha=0.5)
+    plt.xlabel('pik labels')
+    plt.ylabel('predictions')
+    plt.xlim(0,1)
+    plt.ylim(0,1)
+    plt.savefig(figure_path)
+    
+    plot_calibration(labels, preds, num_bins=10, file_name=figure_path.replace('.png', '_calibration.png'))
