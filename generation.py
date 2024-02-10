fix: avoid mem leakage in hidden states generation

generation.py
@@ -51,8 +51,15 @@ def setup_model(args):
 
 def get_hidden_states(model:Model, text_inputs, args):
     if args.mlp:
-        return model.get_batch_MLP_activations(text_inputs,
-                                         keep_all=args.keep_all_hidden_layers)
+        full_hidden_states = []
+        for batch_texts in tqdm(range(0, len(text_inputs), 32*100), 
+                                desc='Generating hidden states',
+                                total=len(text_inputs)//(32*100),
+                                ncols=100):
+            batch_texts = text_inputs[batch_texts:batch_texts+32*100]
+            batch_hidden_states = model.get_batch_MLP_activations(batch_texts, keep_all=args.keep_all_hidden_layers)
+            full_hidden_states.append(batch_hidden_states)
+        return torch.cat(full_hidden_states, dim=0)
     else:
         return model.get_batch_hidden_states(text_inputs,
                                          keep_all=args.keep_all_hidden_layers)