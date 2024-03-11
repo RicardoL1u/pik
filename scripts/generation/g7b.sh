088d4ebf44794dbaae8a19cd70749443482af3a4
RicardoL1u <2018212874@bupt.edu.cn>
2024-03-11 03:42:39 +0000

    ref: mv to generation.py

--- scripts/generation/g7b.sh
+++ scripts/generation/g7b.sh
@@ -1,22 +1,21 @@
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0,1
 
-model=llama-7b-hf
-dataset=gsm8k
-template=cot
-example_file=data/gsm8k/cot_example.json
+model=Mistral-7B-v0.1
+dataset=commonsense_qa
+template=mcq_cmqa
+example_file=data/$dataset/example.json
 datafolder=data/$dataset/$model
 mkdir -p $datafolder
-python generation_lyt.py \
+python generation.py \
     --model_checkpoint MODELS/$model \
     --n_answers_per_question 30 \
     --temperature 1 \
     --pad_token_id 50256 \
     --keep_all_hidden_layers \
-    --hidden_states_filename $datafolder/mlp_act_$template.pt \
+    --hidden_states_filename $datafolder/hidden_states_$template.pt \
     --text_generations_filename $datafolder/text_generations_$template.json \
     --template $template \
     --dataset $dataset \
     --example_file $example_file \
     --max_new_tokens 96 \
-    --mlp \
-    --debug
+    --shot 0
