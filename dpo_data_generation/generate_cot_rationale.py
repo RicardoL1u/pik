77b1faa940557843cfbe6e76be1d0d4493afc73f
RicardoL1u <2018212874@bupt.edu.cn>
2024-03-26 17:35:16 +0000

    fea: add args parser for the cot generation

--- dpo_data_generation/generate_cot_rationale.py
+++ dpo_data_generation/generate_cot_rationale.py
@@ -6,11 +6,38 @@ import re
 from tqdm import tqdm
 from pik.models.model import Model
 import os
+from pathlib import Path
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate COT rationales')
+parser.add_argument('--model_checkpoint', type=str, default='/workspace/MODELS/Qwen1.5-72B-chat', help='Model checkpoint')
+parser.add_argument('--prompt_dir', type=str, default='data/bbh/cot-prompts', help='Directory for COT prompts')
+parser.add_argument('--target_bbh_dir', type=str, default='data/bbh-new', help='Target directory for BBH data')
+parser.add_argument('--debug', action='store_true', help='Debug mode')
+args = parser.parse_args()
+
+model_checkpoint = args.model_checkpoint
+model_name = Path(model_checkpoint).name
+target_bbh_dir = args.target_bbh_dir
+# output dir would be data/bbh-new/{MODEL_NAME}
+output_dir = os.path.join(target_bbh_dir, model_name)
+
+
+if not os.path.exists(output_dir):
+    print(f"Creating output directory: {output_dir}")
+    os.makedirs(output_dir)
+
+print(f"Model checkpoint: {model_checkpoint}")
+print(f"Model name: {model_name}")
+print(f"Target BBH directory: {target_bbh_dir}")
+print(f"Output directory: {output_dir}")
+
 
 
 # Load the model
 llm = Model(
-    model_checkpoint='MODELS/Qwen/Qwen1.5-72B-Chat-GPTQ-Int4',
+    model_checkpoint=args.model_checkpoint,
     generation_options= {
         "max_new_tokens": 512,
         "temperature": 1.0,
@@ -20,7 +47,7 @@ llm = Model(
 )
 
 
-training_files = glob.glob('data/bbh-new/*.json')
+training_files = glob.glob(os.path.join(target_bbh_dir, '*.json'))
 
 training_files = [f for f in training_files if not f.endswith('_prompt.json') and not f.endswith('_result.json')]
 
@@ -33,19 +60,25 @@ for file in training_files:
     print("Task: ", task)
     print("File: ", file)
     dataset = json.load(open(file))
-    # load the cot prompt from data/bbh/bbh/cot-prompts
-    cot_prompt = open(f'data/bbh/cot-prompts/{task}.txt').read()
+    if "examples" in dataset:
+        dataset = dataset["examples"]
+    # load the cot prompt from args.prompt_dir
+    cot_prompt = open(os.path.join(args.prompt_dir, f'{task}.txt')).read()
     
     prompt_list = []
     result_list = []
     
     # check if the result file exists
     do_generate = True
-    if os.path.exists(f'data/bbh_cot/Qwen1.5-72B-chat/{task}_result.json'):
-        print(f"Skipping {task}")
+    if os.path.exists(os.path.join(output_dir, f'{task}.json')):
+        print(f"Do not generate for {task}")
         do_generate = False
-        dataset = json.load(open(f'data/bbh_cot/Qwen1.5-72B-chat/{task}_result.json'))
+        dataset = json.load(open(os.path.join(output_dir, f'{task}.json')))
+        if "example" in dataset:
+            dataset = dataset["example"]
     
+    if args.debug:
+        dataset = dataset[10:20]
     
     for data in tqdm(dataset, desc=f"Processing {file}",ncols=100):
         input_prompt = cot_prompt + '\n\nQ: ' + data['input'] + '\nA: Let\'s think step by step.'
@@ -86,10 +119,17 @@ for file in training_files:
         
     task2accuracy[task] = sum([r['rationale'][0]['is_correct'] for r in dataset]) / len(dataset)
         
-    with open(f'data/bbh_cot/Qwen1.5-72B-chat/{task}_result.json', 'w') as f:
+    with open(os.path.join(output_dir, f'{task}.json'), 'w') as f:
         json.dump(dataset, f, indent=4, ensure_ascii=False)
 
-with open(f'data/bbh_cot/Qwen1.5-72B-chat_accuracy.json', 'w') as f:
-    json.dump(task2accuracy, f, indent=4, ensure_ascii=False)
 
+# convert task2accuracy to pandas dataframe
+df = pd.DataFrame(task2accuracy.items(), columns=['task', 'accuracy'])
+# convert to percentage then round to 2 decimal places
+df['accuracy'] = (df['accuracy'] * 100).round(2)
+print("The accuracy of the model on each task:")
+print(df)
+
+# save to csv
+df.to_csv(os.path.join(output_dir, 'accuracy.csv'), index=False)
 
